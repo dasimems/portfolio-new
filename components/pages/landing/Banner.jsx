4d507a3f92a7bb13821c583343b9a749a4399246
dasimems <isaacseun63@gmail.com>
2023-07-12 12:16:48 +0100

    update: made update to the banner section

--- components/pages/landing/Banner.jsx
+++ components/pages/landing/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
 
             <div className="flex flex-col items-start gap-6">
                 <h1 className="text-2xl sm:text-4xl md:text-6xl font-medium">Hi! I am <span className="font-bold">Duyil Ayomid</span></h1>
-                <p className="text-lg md:text-xl">A web developer, mobile app  and Blockchain Developer</p>
+                <p className="text-lg md:text-xl">A web, mobile app, Blockchain Developer and also an Electrical ELectronics Engineer</p>
                 <Button type="primary">
                     Hire Me
                 </Button>
