0e7a86049968e7e205e22b1b89f74a4857569eaf
dasimems <isaacseun63@gmail.com>
2023-07-14 23:47:51 +0100

    update: added arial label to the project card

--- components/pages/general/works/ProjectCard.jsx
+++ components/pages/general/works/ProjectCard.jsx
@@ -1,9 +1,11 @@
 import Button from "@/components/general/Button";
+import Link from "next/link";
 import { ArrowRight } from "react-iconly";
 
 const ProjectCard = ({ className, type }) => {
   return (
-    <div
+    <Link
+      href=""
       className={`bg-slate-300 cursor-pointer rounded-md relative group/project ${className}`}
     >
       <span className="inline-flex absolute z-[1] top-5 left-5 font-medium text-primary py-2 px-5 text-xs bg-primary-200 rounded-[50px]">
@@ -11,13 +13,15 @@ const ProjectCard = ({ className, type }) => {
       </span>
 
       <div className="absolute bottom-5 left-0 px-7 flex items-center w-full justify-between">
-        <h1 className="font-bold text-slate-800">Project Title</h1>
+        <h1 aria-label="project title" className="font-bold text-slate-800">
+          Project Title
+        </h1>
 
         <Button className="pt-0 group-hover/project:bg-primary group-hover/project:text-white pl-0 pr-0 pb-0 w-[50px] h-[50px] items-center justify-center bg-primary-300 text-primary">
           <ArrowRight set="bold" />
         </Button>
       </div>
-    </div>
+    </Link>
   );
 };
 
