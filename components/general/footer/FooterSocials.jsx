27c84b8d58a8706a89643b7301598fdb0ff17a50
dasimems <isaacseun63@gmail.com>
2023-07-14 22:15:45 +0100

    update: made update to the footer content

--- components/general/footer/FooterSocials.jsx
+++ components/general/footer/FooterSocials.jsx
@@ -1,15 +1,23 @@
-import { socialList } from "@/utils/general"
+import { socialList } from "@/utils/general";
 
 const FooterSocials = () => {
-    return(
-        <ul className="flex flex-wrap items-center gap-6 text-primary-700">
-            {socialList.map(({link, Icon}, index)=>(
-                <li key={index} className="text-lg md:text-xl">
-                    <a href={link} target="_blank" rel="norefferer">{Icon && typeof(Icon) !== "string" && <Icon />}</a>
-                </li>
-            ))}
-        </ul>
-    )
-}
+  return (
+    <ul className="flex flex-wrap items-center gap-6 text-primary-700">
+      {socialList.map(({ link, Icon, name }, index) =>
+        <li key={index} className="text-lg md:text-xl">
+          <a
+            title={`Follow me on ${name}`}
+            href={link}
+            aria-label={`Follow me on ${name}`}
+            target="_blank"
+            rel="norefferer"
+          >
+            {Icon && typeof Icon !== "string" && <Icon />}
+          </a>
+        </li>
+      )}
+    </ul>
+  );
+};
 
 export default FooterSocials;
