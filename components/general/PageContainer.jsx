e3f92ce954e11d93513b61fd3410b3af22f5a89f
dasimems <isaacseun63@gmail.com>
2023-07-15 02:59:23 +0100

    update: remove console.log

--- components/general/PageContainer.jsx
+++ components/general/PageContainer.jsx
@@ -1,21 +1,23 @@
-import { forwardRef, useEffect } from "react"
+import { forwardRef, useEffect } from "react";
 
-const PageContainer = forwardRef(({children, className, innerContent, ...prop}, ref) => {
-
-    useEffect(()=>{
-        console.log(window.innerWidth)
-    }, [])
-    
-    return(
-        <div className={`w-full flex items-center justify-center`}>
-            <div ref={ref} className={`px-[5%] min-[1921px]:px-[80px] w-full ${!innerContent? "max-w-[1920px]" : ""} ${className? className : ""}`} {...prop}>
-                {children}
-            </div>
-
+const PageContainer = forwardRef(
+  ({ children, className, innerContent, ...prop }, ref) => {
+    return (
+      <div className={`w-full flex items-center justify-center`}>
+        <div
+          ref={ref}
+          className={`px-[5%] min-[1921px]:px-[80px] w-full ${!innerContent
+            ? "max-w-[1920px]"
+            : ""} ${className ? className : ""}`}
+          {...prop}
+        >
+          {children}
         </div>
-    )
-})
+      </div>
+    );
+  }
+);
 
-PageContainer.displayName = "PPage Container"
+PageContainer.displayName = "PPage Container";
 
-export default PageContainer
+export default PageContainer;
